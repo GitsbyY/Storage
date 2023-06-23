4710068243fe25ff6fb2703e61f5f207bfa62807
YHJ <dasseiende90@gmail.com>
2023-06-23 21:36:46 +0900

    06230936

---
+++ Ch12DateTest/src/two/CalendarApiTest.java
@@ -0,0 +1,14 @@
+package two;
+
+import java.util.Calendar;
+
+public class CalendarApiTest {
+
+	public static void main(String[] args) {
+	
+		CalendarApi ca = new CalendarApi();
+		
+//		ca.viewDate(2022, 12);
+		ca.viewDate(2023);
+	}
+}
